8e42e111b928187a6c53ff694096edcd00baac34
PlasmaChemist <valrik@gmail.com>
2014-11-19 09:46:10 -0700

    Replaced else statement to default to Kelvin

--- 1_week/2c_convert_challenge.rb
+++ 1_week/2c_convert_challenge.rb
@@ -10,10 +10,10 @@ when 'f'
   kelvin = ((temp -32) / 1.8) + 273.15
 when 'c'
   kelvin = temp + 273.15
-when 'k'
-  kelvin = temp
 when 'r'
   kelvin = ((temp - 491.67) / 1.8) + 273.15
+  else
+    kelvin = temp
 end
 
 case desired_scale
@@ -21,10 +21,10 @@ when 'f'
   converted_temp = ((kelvin - 273.15) * 1.8) + 32
 when 'c'
   converted_temp = kelvin - 273.15
-when 'k'
-  converted_temp = kelvin
 when 'r'
   converted_temp = ((kelvin - 273.15) * 1.8) + 491.67
+  else
+    converted_temp = kelvin
 end
 
 puts "#{converted_temp.round(2)} #{desired_scale.upcase}"
