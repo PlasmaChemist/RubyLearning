385931f355a2f37664fd527f2e332f6f84a484d2
PlasmaChemist <valrik@gmail.com>
2014-11-25 10:28:09 -0700

    Learned about each.with_index !

--- 2_week/1e_split_a_string.rb
+++ 2_week/1e_split_a_string.rb
@@ -1,6 +1,6 @@
 s = 'Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n'
 lines = s.split('\n')
 
-lines.each do |line|
-puts "Line# #{lines.index(line) + 1}: #{line}"
+lines.each.with_index(offset = 1) do |line, index|
+puts "Line# #{index}: #{line}"
 end
