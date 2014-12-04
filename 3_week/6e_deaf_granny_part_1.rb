61e25a345366481d883925b6635a04a415927311
PlasmaChemist <valrik@gmail.com>
2014-12-04 09:16:26 -0700

    converted to a loop

--- 3_week/6e_deaf_granny_part_1.rb
+++ 3_week/6e_deaf_granny_part_1.rb
@@ -1,13 +1,10 @@
-user_statement = nil
-
-while user_statement != 'BYE'
+loop do
   puts 'You say to Granny:'
   user_statement = gets.chomp
-  if user_statement == 'BYE'
-    exit
-  elsif user_statement == user_statement.upcase
-  puts "Granny says: NO, NOT SINCE #{rand(1930..1950)}"
+  break if user_statement == 'BYE'
+  if user_statement == user_statement.upcase
+    puts "Granny says: NO, NOT SINCE #{rand(1930..1950)}"
   else
-  puts 'Granny says: HUH?! SPEAK UP, SONNY!'
+    puts 'Granny says: "HUH?! SPEAK UP, SONNY!"'
   end
 end
